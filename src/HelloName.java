2a9a8f9158c635321900d2578d23eaeebbf4a411
mayweather1993 <spoxer@bigmir.net>
2017-04-15 11:27:55 +0300

    Update HelloName.java

--- src/HelloName.java
+++ src/HelloName.java
@@ -4,6 +4,7 @@
 import java.io.*;
 public class HelloName {
     public static void main (String[] args)throws Exception {
+        System.out.println("Enter your name : ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String name = reader.readLine();
         System.out.println("Hello, " + name);
